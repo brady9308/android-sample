5961d8be1e7a5bff367f8485ea9ae115840eec11
tutu <tutu.liu@cloudtopcn.com>
2016-02-29 18:24:12 +0800

    update file

--- application/src/main/java/silicar/tutu/application/CalendarUtil.java
+++ application/src/main/java/silicar/tutu/application/CalendarUtil.java
@@ -1,7 +1,95 @@
 package silicar.tutu.application;
 
+import android.view.ViewGroup;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
 /**
  * Created by tutu on 2016/2/29.
  */
-public class CalendarUtil {
+public class CalendarUtil extends GregorianCalendar {
+    private long tempTime;
+
+    public static synchronized CalendarUtil getInstance() {
+        return new CalendarUtil();
+    }
+
+    public CalendarUtil() {
+        super();
+    }
+
+    public CalendarUtil(Locale locale) {
+        super(locale);
+    }
+
+    public CalendarUtil(TimeZone timezone) {
+        super(timezone);
+    }
+
+    public CalendarUtil(TimeZone timezone, Locale locale) {
+        super(timezone, locale);
+    }
+
+    public CalendarUtil(int year, int month, int day) {
+        super(year, month, day);
+    }
+
+    public CalendarUtil(int year, int month, int day, int hour, int minute) {
+        super(year, month, day, hour, minute);
+    }
+
+    public CalendarUtil(int year, int month, int day, int hour, int minute, int second) {
+        super(year, month, day, hour, minute, second);
+    }
+
+    /**
+     * 获取一个月所有时间
+     * @return
+     */
+    public long[][] getMonthTimes(){
+        tempTime = getTimeInMillis();
+        int start = 1 - getFirstDayWeek();
+        set(Calendar.DAY_OF_MONTH, 1);
+        add(Calendar.DAY_OF_YEAR, start);
+        long[][] month = new long[6][7];
+        for (int i = 0; i < 6; i++){
+            for (int j = 0; j < 7; j++){
+                month[i][j] = getTimeInMillis();
+                add(Calendar.DAY_OF_YEAR, 1);
+            }
+        }
+        setTimeInMillis(time);
+        return month;
+    }
+
+    /**
+     * 当月第一天星期几
+     * @return
+     */
+    public int getFirstDayWeek(){
+        tempTime = getTimeInMillis();
+        set(Calendar.DAY_OF_MONTH, 1);
+        int week = get(DAY_OF_WEEK);
+        setTimeInMillis(tempTime);
+        return week;
+    }
+
+    /**
+     * 当月最大值
+     * @return
+     */
+    public int getActualMaxDay(){
+        return getActualMaximum(Calendar.DAY_OF_MONTH);
+    }
+
+    public long getTempTime() {
+        return tempTime;
+    }
+
+    public void setTempTime(long tempTime) {
+        this.tempTime = tempTime;
+    }
 }
--- application/src/main/java/silicar/tutu/application/CalendarView.java
+++ application/src/main/java/silicar/tutu/application/CalendarView.java
@@ -1,7 +1,82 @@
 package silicar.tutu.application;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
 /**
+ * 日历选择
  * Created by tutu on 2016/2/22.
  */
-public class CalendarView {
+public class CalendarView extends LinearLayout {
+
+    private View headView;
+    private ViewGroup contentView;
+    private ViewGroup[][] dayView;
+
+    private long[][] dayTime;
+    private CalendarUtil calendarUtil;
+
+    public CalendarView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public CalendarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public CalendarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CalendarView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(Context context){
+        setOrientation(VERTICAL);
+        headView = LayoutInflater.from(context).inflate(R.layout.layout_month_head, this, false);
+        contentView = (ViewGroup) LayoutInflater.from(context).inflate(R.layout.layout_month, this, false);
+        calendarUtil = CalendarUtil.getInstance();
+        addView(headView);
+        addView(contentView);
+        dayView = new ViewGroup[6][7];
+        dayTime = calendarUtil.getMonthTimes();
+        calendarUtil.setTempTime(calendarUtil.getTimeInMillis());
+        for (int i = 0; i < 6; i++){
+            ViewGroup week = getWeekOfMonth(i);
+            getDayOfWeek(week,0);
+            for (int j = 0; j < 7; j++){
+                dayView[i][j] = getDayOfWeek(week, j);
+                calendarUtil.setTimeInMillis(dayTime[i][j]);
+                ((TextView)dayView[i][j].findViewById(R.id.day)).setText("" + calendarUtil.get(Calendar.DAY_OF_MONTH));
+            }
+        }
+        calendarUtil.setTempTime(calendarUtil.getTempTime());
+    }
+
+    public ViewGroup getWeekOfMonth(int week){
+        return (ViewGroup) contentView.getChildAt(week * 2);
+    }
+
+    public ViewGroup getDayOfWeek(ViewGroup weekView, int day){
+        return (ViewGroup) weekView.getChildAt(day * 2);
+    }
+
+    public void showDivision(boolean state){}
+
 }
